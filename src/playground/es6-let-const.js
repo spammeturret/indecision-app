4cae4b68260f3201f380aba384fdb81c9f034b18
unknown <spammeturret@gmail.com>
2019-09-19 21:09:36 +1000

    first commit

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,27 @@
+var nameVar = 'Hiny';
+nameVar = 'Mike';
+console.log('nameVar', nameVar);
+
+let nameLet = 'Jen';
+nameLet = 'Julie';
+console.log('nameLet', nameLet);
+
+const nameConst = 'Frank';
+//nameConst = 'Gunther';
+console.log ('nameConst', nameConst);
+
+function getPetName() {
+    var petName = 'Hal';
+    return petName;
+}
+
+getPetName();
+
+var fullName = 'Hiny Yau'
+
+if (fullName){
+    var firstName = fullName.split(' ')[0];
+    console.log(firstName);
+}
+
+console.log(firstName);
